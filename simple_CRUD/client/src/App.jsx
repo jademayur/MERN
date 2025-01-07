6672455ba37e9a7c3fb772cfbccd96c41062b253
Mayur Jade <jademayur@gmil.com>
2025-01-07 17:49:03 +0530

    insert and get user

---
+++ simple_CRUD/client/src/App.jsx
@@ -0,0 +1,25 @@
+import { useState } from 'react'
+import {BrowserRouter, Routes, Route } from 'react-router-dom'
+import 'bootstrap/dist/css/bootstrap.min.css' 
+import './App.css'
+import CreateUser from './CreateUser'
+import UpdateUser from './UpdateUser'
+import Users from './Users'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Users />}></Route>
+          <Route path="/create" element={<CreateUser />}></Route>
+          <Route path="/update" element={<UpdateUser />}></Route>
+        </Routes>
+      </BrowserRouter>
+    </div>
+  )
+}
+
+export default App
